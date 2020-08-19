5cdea413b27d69a7d5ade2fedcbebacc16c5c4d8
lotukas009 <jurate.lauciute@gmail.com>
2020-08-19 20:35:12 +0300

    uzduotis antra gerimai 2020-08-19

--- index.php
+++ index.php
@@ -1,39 +1,27 @@
 <?php
-
-$distance = rand (10,200);
-$consuptions = 7.5;
-$price_1 = 1.3;
-
-$fuel_total = $distance * ($consuptions/100);
-$price_trp = $price_1 * $fuel_total;
-$li_1 = 'Nuvaziuota distancija: '
-
+$money = rand (0,15);
+$bokal_cost = 3;
+$bokal_num = floor($money/$bokal_cost);
+$total_cost = $bokal_num * $bokal_cost;
+$h2 = "Total: $total_cost Eur";
+$drinks_view = '';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?php print 'As ' .  date('l')  . ' ir PHP'; ?></title>
+    <title>Alus</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"/>
-    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="/style.css" />
 </head>
 <body>
-<main>
-    <section>
-        <h1>Keliones skaiciuokle</h1>
-        <ul>
-            <li><?php print $li_1 . $distance; ?></li>
-            <li>Sunaudota <?php print $fuel_total; ?> l kuro.</li>
-            <li>Kaina: <?php print $price_trp; ?> pinigu.</li>
-        </ul>
-    </section>
-</main>
+<section>
+    <?php for ($i = 1; $i <= $bokal_num; $i++): ?>
+        <div class="drink"></div>
+    <?php endfor; ?>
+</section>
+<h2><?php print $h2; ?></h2>
 </body>
-</html
-
-
-
-
+</html>
 
