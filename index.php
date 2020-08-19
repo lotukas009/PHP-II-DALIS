8285af6ea86d8d37ac58815117fabcce7dd788cd
lotukas009 <jurate.lauciute@gmail.com>
2020-08-19 18:27:36 +0300

    uzduotis pirma keliones kastai 2020-08-19

--- index.php
+++ index.php
@@ -1,58 +1,38 @@
+<?php
+
+$distance = rand (10,200);
+$consuptions = 7.5;
+$price_1 = 1.3;
+
+$fuel_total = $distance * ($consuptions/100);
+$price_trp = $price_1 * $fuel_total;
+$li_1 = 'Nuvaziuota distancija: '
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php print 'As ' .  date('l')  . ' ir PHP'; ?></title>
-    <style>
-        body {
-            margin: 0 auto;
-            width: 50%;
-        }
-
-        .container {
-            min-height: 100vh;
-            display: flex;
-            flex-direction: row;
-            justify-content: center;
-            align-items: center;
-        }
-
-        .container > div:first-child{
-            margin-right: 60px;
-        }
-
-        .bomb-content {
-            background-image: url("https://www.pngkey.com/png/detail/194-1942123_png-small-red-circle.png");
-            width: <?php print 10 * date('s'); ?>px;
-            height: <?php print 10 * date('s'); ?>px;
-            background-size: cover;
-        }
-
-        .explosion-content-00 {
-            background-image: url("https://lh3.googleusercontent.com/proxy/OX0tnlMigoMe0J_B1s-s-dUuIECqELwdeOiM5za6rX9rCkv6C0fkxUnCk5vlTK0FQxi8ctoXgPhSposyuKWl4Ufx684GFwzIAvE-t3Fy0K71DjpacSwm4g");
-            background-size: cover;
-            width: 200px;
-            height: 200px;
-        }
-
-        span {
-            text-align: center;
-            font-size: 30px;
-            font-weight: bold;
-            margin-top: 20px;
-        }
-
-    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"/>
+    <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
-<section class="container">
-    <div class="bomb-content"></div>
-    <span><?php print date('s'); ?></span>
-    <div class="explosion-content-<?php print date('s'); ?>"></div>
-</section>
+<main>
+    <section>
+        <h1>Keliones skaiciuokle</h1>
+        <ul>
+            <li><?php print $li_1 . $distance; ?></li>
+            <li>Sunaudota <?php print $fuel_total; ?> l kuro.</li>
+            <li>Kaina: <?php print $price_trp; ?> pinigu.</li>
+        </ul>
+    </section>
+</main>
 </body>
 </html
+
 
 
 
