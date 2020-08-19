ff2a60b57200043164fb2dde553d19d738e5fa62
lotukas009 <jurate.lauciute@gmail.com>
2020-08-19 07:34:11 +0300

    udzduotis bomba 2020-08-18

--- index.php
+++ index.php
@@ -1,16 +1,44 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Aš valdau HTML <?php print 'ir PHP'; ?></title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php print 'As ' .  date('l')  . ' ir PHP'; ?></title>
+    <style>
+        body {
+            margin: 0 auto;
+            width: 50%;
+        }
+
+        .container {
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .bomb-content {
+            background-image: url("https://www.pngkey.com/png/detail/194-1942123_png-small-red-circle.png");
+            width: <?php print 10 * date('s'); ?>px;
+            height: <?php print 10 * date('s'); ?>px;
+            background-size: cover;
+            align-items: center;
+        }
+
+        span {
+            text-align: center;
+            font-size: 30px;
+            font-weight: bold;
+            margin-top: 20px;
+        }
+
+    </style>
 </head>
 <body>
-<main>
-    <h1>Jūratė <?php print 'yra PHP'; ?> asė</h1>
-    <p>Kuriu dinaminį turinį dabar: Atidaryta <?php print date('Y-m-d H:i:s'); ?></p>
-</main>
+<div class="container">
+    <div class="bomb-content"></div>
+    <span><?php print date('s'); ?></span>
+</div>
 </body>
 </html>
