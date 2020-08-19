b99d2f4f6aa19dce96fc71b8709d81a53332aa71
lotukas009 <jurate.lauciute@gmail.com>
2020-08-19 07:01:27 +0300

    Uzduotis pirma-antra

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aš valdau HTML <?php print 'ir PHP'; ?></title>
+</head>
+<body>
+<main>
+    <h1>Jūratė <?php print 'yra PHP'; ?> asė</h1>
+    <p>Kuriu dinaminį turinį: Atidaryta <?php print date('Y-m-d H:i:s'); ?></p>
+</main>
+</body>
+</html>
