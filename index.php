c424f590db24387240d95d00bf17cb9408c1df05
lotukas009 <jurate.lauciute@gmail.com>
2020-08-21 07:33:32 +0300

    array matrix color 2020-08-20

--- index.php
+++ index.php
@@ -1,41 +1,16 @@
-<?php
-$money = rand (3,27);
-$bokal_cost = 3;
-$bokal_num = floor($money/$bokal_cost);
-$total_cost = $bokal_num * $bokal_cost;
-$timestamp = time() + rand(20, 30) * 60; //sekundės
+<?PHP
+
+$matrix = [
+    ['green','green','green','green','green','green','green','green'],
+    ['green','yellow','yellow','yellow','yellow','yellow','yellow','green'],
+    ['green','yellow','red','red','red','red','yellow','green'],
+    ['green','yellow','red','yellow','yellow','red','yellow','green'],
+    ['green','yellow','red','yellow','yellow','red','yellow','green'],
+    ['green','yellow','red','red','red','red','yellow','green'],
+    ['green','yellow','yellow','yellow','yellow','yellow','yellow','green'],
+    ['green','green','green','green','green','green','green','green'],
+];
+
+var_dump($matrix);
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Drinks</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"/>
-    <link rel="stylesheet" href="/style.css"/>
-</head>
-<body>
-<section>
-    <div>
-        <?php for ($m = 3; $m <= $total_cost; $m += $bokal_cost, $timestamp += rand(20, 30) * 60): ?>
-        <div class="column">
-            <div>
-                <span><?php print date('H:i', $timestamp); ?></span>
-                <span><?php print "$m Eur"; ?></span>
-            </div>
-            <div class="rows">
-                <?php for ($i = $m; $i > 0; $i -= $bokal_cost): ?>
-                    <div class="drink <?php print $i == 3 ? 'full' : 'empty'; ?>"></div>
-                <?php endfor; ?>
-            </div>
-        </div>
-        <?php endfor; ?>
-    </div>
-</section>
-</body>
-</html>
-
-
-
-
