6abbc0bc23997399385d1ee0b30b14d596afb0a2
lotukas009 <jurate.lauciute@gmail.com>
2020-08-26 20:03:25 +0300

    Forms 'number squared'

--- index.php
+++ index.php
@@ -1,37 +1,22 @@
 <?php
-$game = [
-    'objects' =>[
-    [
-        'x' => 500,
-        'y' => 200,
-        'class' => 'light_red',
-        'on_fire' => true,
-    ],
-    [
-        'x' => 550,
-        'y' => 250,
-        'class' => 'light_yellow',
-        'on_fire' => true,
-    ],
-    [
-        'x' => 600,
-        'y' => 300,
-        'class' => 'light_green',
-         'on_fire' => true,
-    ],
-    [
-        'x' => 650,
-        'y' => 350,
-        'class' => 'oven',
-        'on_fire' => true,
-    ],
-   ],
-];
-//var_dump($game);
-//foreach ($game['objects'] as  $value){
-//   print$value['x'];
-//
-//}
+/**
+// * Squares the number
+// * @param int $x number
+// * @return int squared number
+// */
+function square($x)
+{
+    return $x ** 2;
+}
+var_dump($_POST);
+
+
+if (isset($_POST['enterNumber']) && is_numeric($_POST['enterNumber'])) {
+    $answer = 'The result is following: ' . square($_POST['enterNumber']);
+} else {
+    $answer = 'Please enter the number';
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -39,19 +24,25 @@ $game = [
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Drinks</title>
+    <title>BOOKS</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"/>
-    <link rel="stylesheet" href="/style.css"/>
+    <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
 <main>
-    <div><?php foreach ($game['objects'] as $value): ?>
-    <h2><?php print $value['class']; ?></h2>
-        <?php endforeach ?>
-    </div>
-<!--    <div>-->
-<!--            <h2>--><?php //print $game['objects'][0]['class']; ?><!--</h2>-->
-<!--    </div>-->
-</main>
+    <form method="POST">
+        <div><p>Number squared</p></div>
+        <div>
+            <label for="skaicius">Enter number:</label>
+            <input type="number" name="enterNumber" id="skaicius">
+            <button name="submit">Submit</button>
+        </div>
+        <div>
+          <p><?=$answer?></p>
+        </div>
+       </main>
 </body>
 </html>
+
+
+
